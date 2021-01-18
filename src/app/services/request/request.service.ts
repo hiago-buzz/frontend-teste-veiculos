1dda59801b7c5ad4ab4375e825669ed8ee99443c
Cicero Hiago Santos <CHISANTOS>
2021-01-18 14:02:03 -0300

    refatorar modulo request

--- src/app/services/request/request.service.ts
+++ src/app/services/request/request.service.ts
@@ -8,12 +8,12 @@ import { Observable } from 'rxjs';
 export class RequestService {
 
   constructor(
-    private baseUrl: string = `http://www.fipeapi.appspot.com/api/1/`,
+    private baseUrl: string = process.env.BASE_URL,
     private http: HttpClient,
-    private format: string = '.json'
+    private format: string = 'json'
   ) { }
 
   public getter(url: string): Observable<any> {
-    return this.http.get(this.baseUrl + url + this.format);
+    return this.http.get(`${this.baseUrl}/api/1/${url}.${this.format}`);
   }
 }
